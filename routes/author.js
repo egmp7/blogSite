e8c95f9662e38971437475a105c808e33964742d
egmp7 <deweyerick@gmail.com>
2023-07-18 13:18:33 -0500

    edit page, save articles router

--- routes/author.js
+++ routes/author.js
@@ -56,7 +56,6 @@ router.post("/edit-settings", (req, res, next) => {
         next(err); 
       } else {
         res.redirect("/author");
-        next();
       }
     }
   );
@@ -79,13 +78,47 @@ router.get("/create-new-draft", createDraft, (req, res, next) => {
 });
 
 /**
+ * @def Saves article changes from the edit page
+ */
+router.post("/save-article/:id",(req, res, next)=>{
+  
+  const data = req.body;
+
+  global.db.all(`UPDATE articles SET title = "${data.articleTitle}", 
+                                    subtitle = "${data.articleSubtitle}", 
+                                    author = "${data.articleAuthor}", 
+                                    article_text = "${data.articleText}",
+                                    last_modified_date = "${Date.now()}"
+                                    WHERE article_id = ${req.params.id}`, 
+  function (err, data) {
+    if (err) {
+      next(err); 
+    } 
+    else {
+      res.redirect("/author")
+    }
+  });
+})
+
+/**
  * @def Dynamic Edit article page
  */
 router.get("/edit/:id", (req, res, next) => {
 
-  res.render("edit.ejs", {
-    title: "Edit page"
-  });  
+  global.db.all(`SELECT article_id, title,subtitle,author,article_text FROM articles WHERE article_id = ${req.params.id}`,
+    function (err, data) {
+    if (err) {
+      next(err); 
+    } 
+    else {
+
+      const article = data[0]
+      res.render("edit.ejs", {
+        title: "Edit page",
+        article
+      });  
+    }
+  });
 });
 
 /**
@@ -93,7 +126,14 @@ router.get("/edit/:id", (req, res, next) => {
  */
 function createDraft(req,res,next) {
   global.db.run(
-    "INSERT INTO articles (title,subtitle,article_text,author,likes,published,creation_date,last_modified_date) VALUES( ?, ?, ?, ?, ?, ?, ?, ? );",
+    `INSERT INTO articles (title,
+                          subtitle,
+                          article_text,
+                          author,likes,
+                          published,
+                          creation_date,
+                          last_modified_date) 
+    VALUES( ?, ?, ?, ?, ?, ?, ?, ? );`,
     ["title", "subtitle","text","author",0,0,Date.now(),Date.now()],
     function (err) {
       if (err) {
